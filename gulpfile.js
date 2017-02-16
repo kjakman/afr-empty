4e96d934c842af51d939aaab8aacaa5552f6d56d
Kjetil Larsen <github@kjakman.com>
2017-02-16 22:52:28 +0000

    first day

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+var elixir = require('laravel-elixir');
+elixir.config.assetsPath = 'resources';
+elixir.config.publicPath = 'public';
+elixir.config.css.outputFolder = 'resources/css';
+elixir.config.js.outputFolder = 'resources/js';
